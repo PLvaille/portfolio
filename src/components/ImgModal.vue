d551ef32f27c6fde1446ee2d7c2bc5b9147a3944
PLvaille <pierrelouis.vaille@gmail.com>
2022-07-14 16:39:40 +0200

    creation de la modale

--- src/components/ImgModal.vue
+++ src/components/ImgModal.vue
@@ -3,15 +3,10 @@
         <div class="modal-wrapper">
             <div class="modal-container">
                 
-                <div class="carouselBtnContainer">
-                <button class="carouselBtn carouselPreviousImg">⬅️</button>             
-                <button class="carouselBtn carouselNextImg">➡️</button>
-                </div>
-
-                <div class="modal-body">{{carousel[0]}}</div>
-                
-                <button class="carouselBtn carouselCloseBtn" @click="$parent.displayImg()">X</button>
-
+                <button class="carouselBtn carouselBtnPrev">⬅️</button>             
+                <button class="carouselBtn carouselBtnNext">➡️</button>
+                <button class="carouselBtn carouselBtnClose" @click="$parent.displayImg()">X</button>
+                 <div class="modal-body"><img :src="require(`../assets/img/${carousel[0]}`)"/></div>
             </div>
         </div>
     </div>
@@ -20,16 +15,41 @@
 <script>
 export default {
 
+    // v if id = id
+
     //transformer le tableau pour récup chaque image
 
     props : {
-        carousel : []
+        carousel: {
+            type : Array
+        },
+        // id : {
+        //     type: Number
+        // }
+    },
+    // data(){
+    //     imgtodisplay = []
+    // },
+    methods:{
+        log(){
+            console.log(this.carousel)
+           // this.imgtodisplay = carousel[0];
+        }
+    },
+    created(){
+        this.log();        
     }
 
 }
 </script>
 
-<style>
+<style lang="scss">
+$dark-main: #475C7A;
+$grey : #685D79;
+$darkred : #AB6C82;
+$red : #D8737F;
+$orange : #FCB860;
+$cream : #dbd5ce;
 
 .modal-mask {
   position: fixed;
@@ -49,16 +69,53 @@ export default {
 }
 
 .modal-container {
-    display:flex;
+    /* display:flex;
     flex-direction: column;
-    justify-content: center;
-  width: 300px;
+    justify-content: center; */
+  width: 83%;
   margin: 0px auto;
   padding: 20px 30px;
   background-color: #fff;
   border-radius: 2px;
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.33);
   transition: all 0.3s ease;
+  position:relative;
+}
+
+.modal-body {
+    width:80%;
+    & img {
+        width:60%;
+    }
+}
+
+.carouselBtn {
+cursor:pointer;
+border-radius: 4px;
+}
+.carouselBtnNext{
+    position:absolute;
+    top:50%;
+    right:8px;
+}
+ .carouselBtnPrev {
+       position:absolute;
+    top:50%;
+    left:8px;
+
+}
+
+.carouselBtnClose {
+    position:absolute;
+    top:8px;
+    right: 8px;
+    background: $darkred;
+    box-shadow: #685D79 2Px 1px;
+    &:hover {
+        
+        background:$red;
+        box-shadow: #685D79 1px 0px;
+    }
 }
 
 </style>
